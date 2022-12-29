15cbc7c871b9de857ffde4055c1805dac7f3b52b
zijia chen <49104556+nirvanaczj@users.noreply.github.com>
2022-12-29 11:42:23 +0800

    Updates

---
+++ arduino_secrets.h
@@ -0,0 +1,3 @@
+#define SECRET_SSID "surplex-io"
+#define SECRET_PASS "abcd1234"
+#define SECRET_ADDRESS "192.168.0.101"
--- surplex_firmware_public.ino
+++ surplex_firmware_public.ino
@@ -12,15 +12,13 @@ char pass[] = SECRET_PASS;
 
 char serverAddress[] = SECRET_ADDRESS;
 
-if (SECRET_SIDE == 0){
-  int send_id = 3;// Left
-  int port = SECRET_PORT;// Left
-  byte ps_data[251] = {byte(3)}; // Left}
-else{
-  int send_id = 2; //Right
-  int port = SECRET_PORT-1; // Right
-  byte ps_data[251] = {byte(2)};// Right
-  }
+int send_id = 3;// Left
+int port = 12100;// Left
+byte ps_data[251] = {byte(3)}; // Left}
+
+// int send_id = 2; //Right
+// int port = 12101; // Right
+// byte ps_data[251] = {byte(2)};// Right
 
 int lines_ct = 3;
 int decoders_ct = 8;
